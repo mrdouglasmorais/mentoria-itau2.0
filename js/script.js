951d4c446ae14036106b5deba25be4f88642b159
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-05-20 17:58:08 -0300

    formulário

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function clickTest(){
-  alert('Olá academia itaú')
-}
+let formulario = document.getElementById('formulario')
+
+formulario.addEventListener('submit', function(e){
+  e.preventDefault()
+  let nome = document.getElementById('nome').value
+  let email = document.getElementById('email').value
+  let telefone = document.getElementById('telefone').value
+  
+  console.log({ nome, email, telefone})
+})
